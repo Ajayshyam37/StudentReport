fd4d89ad21dff0240de868af7dc0f3f1c69ca676
Ajay <ajayshyam88@gmail.com>
2020-10-18 12:46:34 +0530

    Initial Project Layout

---
+++ StudentReport/Subjects.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentReport
+{
+    public class Subjects
+    {
+        public int Chemistry;
+        public int Physics;
+        public int Biology;
+        public int Social;
+        public int Mathametics;
+        public int Computers;
+
+    }
+}
