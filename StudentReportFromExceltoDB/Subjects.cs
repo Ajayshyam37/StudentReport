a61cf7880f305ef3b5c17304a9a713540f309a6d
Ajay <ajayshyam88@gmail.com>
2020-10-18 12:48:44 +0530

    Student Report For Excel Sheets

---
+++ StudentReportFromExceltoDB/Subjects.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentReportFromExceltoDB
+{
+    public class Subjects
+    {
+        public int Chemistry;
+        public int Physics;
+        public int Biology;
+        public int Social;
+        public int Mathametics;
+        public int Computers;
+
+    }
+}
